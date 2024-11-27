61eb59238ee3503e328f9d2470167ba9ef304105
Subhradeep <subhradeepbasu2002@gmail.com>
2024-11-27 08:43:33 +0530

    Add route to serve index.html

--- server/app.js
+++ server/app.js
@@ -20,4 +20,8 @@ app.use('/api/tasks', taskRouter);
 
 app.use(express.static('public'))
 
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('public/index.html'));
+});
+
 export default app;
