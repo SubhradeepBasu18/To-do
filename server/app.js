2a0b4dfe346743f579f6a4204df99a79845b2dbb
Subhradeep <subhradeepbasu2002@gmail.com>
2024-11-27 09:19:50 +0530

    Test Commit 3

--- server/app.js
+++ server/app.js
@@ -11,10 +11,10 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '../public')))
 
 app.get('/',(req,res) =>{
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+    res.sendFile(path.join(__dirname, '../public', 'index.html'))
 })
 
 app.use(cors(
